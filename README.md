a95de75fb0eaae1a51add621051cd15ce63c0665
KSL <kslagdive@gmail.com>
2013-11-21 00:03:00 +0530

    blog

---
+++ README.md
@@ -0,0 +1,5 @@
+blog
+====
+
+Rails blog
+https://www.codeship.io/projects/edfaa080-2039-0131-b322-724bbebb2092/status
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '460a4a7f823f2d6844b360db7fb0aca99fca492e23d216d016c723cb8211fa5e515082deed5fcbfd64242f30362a3a43c12c8004b4dfbb8b9a84ea01d89044cb'
